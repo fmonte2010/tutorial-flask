43fd47a8085930fca7f49e881359b0cc3a4bfa9b
FaustoMonte <fausto_montenegro@hotmail.com>
2020-12-29 20:10:46 -0500

    2do commit se crean las plantillas y se renderizan desde el programa principal de la app

--- run.py
+++ run.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+posts = []
+
+@app.route("/")
+def index():
+    return render_template("index.html", num_posts=len(posts))
+
+@app.route("/p/<string:slug>/")
+def show_post(slug):
+    return render_template("post_view.html", slug_title=slug)
+
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>/")
+def post_form(post_id=None):
+    return render_template("admin/post_form.html", post_id=post_id)
+
+@app.route("/signup/", methods=["GET", "POST"])
+def show_signup_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        next = request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('index'))
+    return render_template("signup_form.html")
