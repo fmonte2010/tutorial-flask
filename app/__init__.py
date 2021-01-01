2a06a6abb51499291985e5508c1a75a9ee951db2
FaustoMonte <fausto_montenegro@hotmail.com>
2021-01-01 10:09:35 -0500

    7mo commit se ha añadido flask-migrate que me permitira actualizar la base de datos

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import logging
 
 login_manager = LoginManager()
 db = SQLAlchemy()
+migrate = Migrate()  # Se crea un objeto de tipo Migrate
+
 def create_app(settings_module):
     app = Flask(__name__, instance_relative_config=True)
 
@@ -22,6 +25,7 @@ def create_app(settings_module):
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
     db.init_app(app)
+    migrate.init_app(app, db)  # Se inicializa el objeto migrate
 
     # Registro de los Blueprints
     from .auth import auth_bp
